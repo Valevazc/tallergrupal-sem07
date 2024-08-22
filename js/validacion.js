fe579a2056fb6873e0d61a3737897b78d1c47bdb
guillerodriguez09 <rodriguezguillermina816@gmail.com>
2024-08-22 19:52:47 -0300

    archivos1

--- js/validacion.js
+++ js/validacion.js
@@ -6,12 +6,6 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
-document.getElementById('regBtn').addEventListener("click" , function(event){
-
-    controlCampos();
-    
-    });
-
 function controlCampos(){
 
 let nom = document.getElementById('nombre');
@@ -19,30 +13,12 @@ let ape = document.getElementById('apellido');
 let email = document.getElementById('email');
 let pass1 = document.getElementById('password1');
 let pass2 = document.getElementById('password2');
-let terminos = document.getElementById('terminos').checked;
 
 if(nom.value == ""||ape.value== ""||email.value==""||pass1.value==""||pass2.value ==""){
     showAlertError();
     return;
    
-}
-  if (pass1.value.length<6){
-        showAlertError();
-        return;
-        
-    }
-        if(pass1.value !== pass2.value){
-            showAlertError();
-            return;
-            
-        }
-            if(!terminos){
-                showAlertError();
-                return;
-                
-            }
-                showAlertSuccess();
-            }
+} }
         
 
 
